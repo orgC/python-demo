1f941d741d34053e56645099f116af57f3b0a604
Thomas Chen <thomas.cjk@gmail.com>
2019-08-27 17:23:14 +0800

    modify port

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 import socket
 
 HOST = '0.0.0.0'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+PORT = 80 # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
